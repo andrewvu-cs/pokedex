2ce440d8c591f5f71d00156fe313b0189f5b1b3e
avu916 <andrewvu.cs@gmail.com>
2019-05-25 10:10:38 -0700

    Updates

---
+++ precache-manifest.d932e0d15c130cb928019923dbc5083e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf84cf041a7e618f74cb1a3ce8e2549a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d982ae8e225ce999ae75",
+    "url": "/pokedex/static/css/main.4231bc0f.chunk.css"
+  },
+  {
+    "revision": "d3963d4373159d2d036b",
+    "url": "/pokedex/static/js/2.4c73ce8d.chunk.js"
+  },
+  {
+    "revision": "d982ae8e225ce999ae75",
+    "url": "/pokedex/static/js/main.8c664a8c.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1402ebebd75799304c6b88ae3cbbc0c8.js"
+  "/pokedex/precache-manifest.d932e0d15c130cb928019923dbc5083e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
